7db431188a8b723e18b646bc98d64231576e4a1a
javakarthicklog <javakarthiklog@gmail.com>
2017-08-31 07:27:55 +0530

    second commit

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/map';
 
     weatherUsers(location:string){
           if(location){
+            console.log('http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&appid=8688562469a983f79d7fa231a8971934')
          return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&appid=8688562469a983f79d7fa231a8971934' ).map(res => res.json());
       }
     } 
